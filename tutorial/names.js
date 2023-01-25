f14ea591bd92da74d4a386a177b34eca39c7be10
Zeynep Elmacı <94968009+zeynepelmacii@users.noreply.github.com>
2023-01-25 22:01:04 +0300

    Delete tutorial directory

--- tutorial/names.js
+++
@@ -1,8 +0,0 @@
-//local
-const secret = 'SUPER SECRET';
-//share
-const name1 = 'john';
-const name2 = 'peter';
-
-
-module.exports={name1,name2}
--- tutorial/utils.js
+++
@@ -1,9 +0,0 @@
-const sayHi = (name) => {
-    console.log(`Hello there ${name}!`);
-}
-
-//direkt erişim var kullanılan taraftaki değişkeni kullanarak
-module.exports = sayHi;
-
-//bu şekilde yapsaydık say helloyu kullarak erişebilidrik
-//module.exports.sayHellooo = sayHi;
